7451ff2d9705ff7cb861c5907e6036312eeee0eb
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2024-08-06 20:35:41 +0200

    refactor: throw exception if read variable size is smaller than 1

--- lynx-journal/src/main/java/pl/wsztajerowski/journal/records/RecordReadChannel.java
+++ lynx-journal/src/main/java/pl/wsztajerowski/journal/records/RecordReadChannel.java
@@ -5,6 +5,7 @@ import pl.wsztajerowski.journal.exceptions.InvalidRecordHeader;
 import pl.wsztajerowski.journal.exceptions.JournalRuntimeIOException;
 import pl.wsztajerowski.journal.exceptions.NotEnoughSpaceInBuffer;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -12,6 +13,7 @@ import java.nio.file.Path;
 
 import static java.nio.file.StandardOpenOption.CREATE;
 import static java.nio.file.StandardOpenOption.READ;
+import static pl.wsztajerowski.journal.records.RecordHeader.RECORD_PREFIX;
 import static pl.wsztajerowski.journal.records.RecordHeader.recordHeaderLength;
 
 public class RecordReadChannel {
@@ -40,29 +42,33 @@ public class RecordReadChannel {
         ByteBuffer localCopyOfDestination = destination
             .duplicate()
             .limit(destination.position() + recordHeader.variableSize());
-        var variableBuffer = readFromFileChannel(localCopyOfDestination, location.offset() + recordHeaderLength());
+        var variableBuffer = readFromFileChannel(localCopyOfDestination, location.offset() + recordHeaderLength(), recordHeader.variableSize());
         return new Record(recordHeader, location, variableBuffer);
     }
 
     private RecordHeader validateAndGetRecordHeader(Location location) {
         // v01 record header format: [ int prefix, int variableSize ]
         recordHeaderBuffer.clear();
-        var headerBuffer = readFromFileChannel(recordHeaderBuffer, location.offset());
+        var headerBuffer = readFromFileChannel(recordHeaderBuffer, location.offset(), recordHeaderLength());
         int prefix = headerBuffer.getInt();
         int variableSize = headerBuffer.getInt();
-        if (prefix != RecordHeader.RECORD_PREFIX) {
+        if (prefix != RECORD_PREFIX || variableSize < 1 ) {
             throw new InvalidRecordHeader(prefix, variableSize);
         }
         return new RecordHeader(variableSize);
     }
 
-    private ByteBuffer readFromFileChannel(ByteBuffer buffer, long offset) {
+    private ByteBuffer readFromFileChannel(ByteBuffer buffer, long offset, int expectedSize) {
         try {
-            fileChannel.read(buffer, offset);
+            int readBytes = fileChannel.read(buffer, offset);
+            if (readBytes == -1) {
+                throw new JournalRuntimeIOException("Read from outside of channel", new EOFException());
+            } else if (readBytes != expectedSize) {
+                throw new JournalRuntimeIOException("Number of read bytes from channel (%d) is different than expected (%d)".formatted(readBytes, expectedSize));
+            }
+            return  buffer.flip();
         } catch (IOException e) {
             throw new JournalRuntimeIOException("Error during reading from fileChannel", e);
         }
-        buffer.rewind();
-        return buffer;
     }
 }
