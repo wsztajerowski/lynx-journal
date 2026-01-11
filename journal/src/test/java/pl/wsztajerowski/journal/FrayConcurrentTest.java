a61dd3d8b6e8210105575f08f1da3139dc5b7177
Wiktor-Sztajerowski <wiktor.sztajerowski@gmail.com>
2026-01-11 17:18:02 +0100

    test: enhance FrayConcurrentTest with additional concurrency scenarios and improve file handling setup

--- journal/src/test/java/pl/wsztajerowski/journal/FrayConcurrentTest.java
+++ journal/src/test/java/pl/wsztajerowski/journal/FrayConcurrentTest.java
@@ -1,24 +1,21 @@
 package pl.wsztajerowski.journal;
 
-import com.google.common.jimfs.Configuration;
-import com.google.common.jimfs.Jimfs;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.pastalab.fray.junit.junit5.FrayTestExtension;
 import org.pastalab.fray.junit.junit5.annotations.ConcurrencyTest;
+import pl.wsztajerowski.journal.records.RecordHeader;
 
 import java.io.IOException;
-import java.nio.file.FileSystem;
 import java.nio.file.Path;
 
+import static java.nio.file.Files.createTempFile;
 import static java.nio.file.Files.readAllBytes;
 import static org.assertj.core.api.Assertions.assertThat;
 import static pl.wsztajerowski.journal.BytesTestUtils.toUpperCaseHexString;
 import static pl.wsztajerowski.journal.BytesTestUtils.toUpperCaseUtf8HexString;
-import static pl.wsztajerowski.journal.FilesTestUtils.wrapInJournalByteBufferWithSize;
+import static pl.wsztajerowski.journal.FilesTestUtils.*;
+import static pl.wsztajerowski.journal.records.JournalByteBufferFactory.createJournalByteBuffer;
 import static pl.wsztajerowski.journal.records.RecordTestUtils.recordHeaderPrefixInHexString;
 
 @ExtendWith(FrayTestExtension.class)
@@ -27,19 +24,37 @@ class FrayConcurrentTest {
     public static final int BATCH_SIZE = 64;
     private Journal sut;
     private Path dataFilePath;
-    private FileSystem fileSystem;
 
     @BeforeEach
-    void setUp() {
-        fileSystem = Jimfs.newFileSystem(Configuration.unix());
-        dataFilePath = fileSystem.getPath("/test-concurrent-fray.journal");
+    void setUp() throws IOException {
+        dataFilePath = createTempFile("journal", ".dat");
         sut = Journal.open(dataFilePath, false, BATCH_SIZE);
     }
 
     @AfterEach
     void tearDown() throws IOException {
         sut.close();
-        fileSystem.close();
+    }
+
+    @ConcurrencyTest
+    void read_after_flushed_writes_by_closing_journal() throws IOException {
+        // given
+        var firstVariableContent = "My";
+        var secondVariableContent = "project!";
+
+        // when
+        var firstVariableLocation = sut.writeAsync(wrapInJournalByteBuffer(firstVariableContent));
+        var secondVariableLocation = sut.writeAsync(wrapInJournalByteBuffer(secondVariableContent));
+
+        // and
+        sut.close();
+        sut = Journal.open(dataFilePath, false, BATCH_SIZE);
+
+        // then
+        var secondReadContent = readAsUtf8(sut.read(createJournalByteBuffer(64), secondVariableLocation));
+        var firstReadContent = readAsUtf8(sut.read(createJournalByteBuffer(64), firstVariableLocation));
+        assertThat(String.join(" ", firstReadContent, secondReadContent))
+            .isEqualTo("My project!");
     }
 
     @ConcurrencyTest
@@ -49,26 +64,18 @@ class FrayConcurrentTest {
         var secondVariableContent = "project!";
 
         // when
-        var firstVariableLocation = sut.writeAsync(wrapInJournalByteBufferWithSize(firstVariableContent, BATCH_SIZE));
-        var secondVariableLocation = sut.writeAsync(wrapInJournalByteBufferWithSize(secondVariableContent, BATCH_SIZE));
-//
-//        // and
-//        var secondReadContent = readAsUtf8(sut.read(createJournalByteBuffer(64), secondVariableLocation));
-//        var firstReadContent = readAsUtf8(sut.read(createJournalByteBuffer(64), firstVariableLocation));
-
-        // then
+        sut.writeAsync(wrapInJournalByteBufferWithSize(firstVariableContent, BATCH_SIZE));
+        sut.write(wrapInJournalByteBufferWithSize(secondVariableContent, BATCH_SIZE));
 
         // then
         assertThat(readAllBytes(dataFilePath))
             .asHexString()
-            .containsSequence(
+            .containsSubsequence(
                 recordHeaderPrefixInHexString(),
-                toUpperCaseHexString(BATCH_SIZE),
+                toUpperCaseHexString(BATCH_SIZE - RecordHeader.recordHeaderLength()),
                 toUpperCaseUtf8HexString(firstVariableContent),
                 recordHeaderPrefixInHexString(),
-                toUpperCaseHexString(BATCH_SIZE),
+                toUpperCaseHexString(BATCH_SIZE - RecordHeader.recordHeaderLength()),
                 toUpperCaseUtf8HexString(secondVariableContent));
-//        assertThat(String.join(" ", firstReadContent, secondReadContent, thirdReadContent))
-//            .isEqualTo("My fantastic project!");
     }
 }
